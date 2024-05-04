0e2c4481b0b672d1da1400002c619a2d71a98e05
Pierrick Réveillère <reveillere.pierrick@gmail.com>
2024-05-04 14:24:33 +0200

    feat: worm sliding movement

--- Assets/Scripts/InputWorm.cs
+++ Assets/Scripts/InputWorm.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class InputWorm : MonoBehaviour
 {
   private AudioSource audioSource;
-  private bool hasBeenReleased = true;
-  private float lastMoveTime = 0;
-  private float MOVE_COOL_DOWN = 0.3f;
+  //private bool hasBeenReleased = true;
+  //private float lastMoveTime = 0;
+  //private float MOVE_COOL_DOWN = 0.3f;
+  private float MOVE_SPEED = 6f;
 
   // Start is called before the first frame update
   void Start()
@@ -19,23 +20,35 @@ public class InputWorm : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (hasBeenReleased)
-      {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        if (moveHorizontal != 0)
-        {
-          // Moving.
-          transform.position = transform.position + new Vector3(moveHorizontal, 0, 0);
-          audioSource.Play();
-          lastMoveTime = Time.time;
-          hasBeenReleased = false;
-        }
-      }
+    //if (hasBeenReleased)
+    //{
+    //  float moveHorizontal = Input.GetAxisRaw("Horizontal");
+    //  if (moveHorizontal != 0)
+    //  {
+    //    // Moving.
+    //    transform.position = transform.position + new Vector3(moveHorizontal, 0, 0);
+    //    audioSource.Play();
+    //    lastMoveTime = Time.time;
+    //    hasBeenReleased = false;
+    //  }
+    //}
 
-      // Reset input if buttons are released.
-      if (/*Input.GetAxisRaw("Horizontal") == 0 || */(Time.time - lastMoveTime) > MOVE_COOL_DOWN)
+    //// Reset input if buttons are released.
+    //if (/*Input.GetAxisRaw("Horizontal") == 0 || */(Time.time - lastMoveTime) > MOVE_COOL_DOWN)
+    //{
+    //  hasBeenReleased = true;
+    //}
+    float moveHorizontal = Input.GetAxisRaw("Horizontal");
+    transform.position = transform.position + new Vector3(moveHorizontal * Time.deltaTime * MOVE_SPEED, 0, 0);
+    if (Input.GetAxisRaw("Horizontal") != 0)
+    {
+      if (!audioSource.isPlaying)
       {
-        hasBeenReleased = true;
+        audioSource.Play();
       }
+    } else
+    {
+      audioSource.Stop();
     }
+  }
 }
