d1914813e286d295c5da5b44be07e4fdeb3c66d7
Amalia <amalia.pluchard@gmail.com>
2024-05-04 21:30:01 +0200

    update: need to make a function shitmud

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,8 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 using UnityEngine.Tilemaps;
+
+
+public enum MudState
+{
+    Eat,
+    Shit,
+}
+
 
 public class PlayerCollision : MonoBehaviour
 {
@@ -10,27 +21,54 @@ public class PlayerCollision : MonoBehaviour
 
     public Tile normalTile;
     public GameObject worm;
+    public MudState mudState = MudState.Eat;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (this.mudState == MudState.Shit)
+        {
+            this.ShitMud();
+        }
+    }
 
+    private void OnMouseDown()
+    {
+        Debug.Log("click click");
+        this.ChangeMudState();
+    }
+
+    private void ChangeMudState()
+    {
+        if (this.mudState == MudState.Eat)
+        {
+            this.mudState = MudState.Shit;
+            this.spriteRenderer.color = Color.cyan;
+        }
+        else if (this.mudState == MudState.Shit)
+        {
+            this.mudState = MudState.Eat;
+            this.spriteRenderer.color = Color.red;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        EatMud(collision);
+        if (this.mudState == MudState.Eat)
+            EatMud(collision);
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        EatMud(collision);
+        if (this.mudState == MudState.Eat)
+            EatMud(collision);
     }
 
     private void EatMud(Collision2D collision)
@@ -44,6 +82,11 @@ public class PlayerCollision : MonoBehaviour
             Vector3 scaledDirection = direction * 0.1f;
             collision.gameObject.GetComponent<TileModifier>()?.DigHole(coordinates + scaledDirection);
         }
+    }
+
+    private void ShitMud()
+    {
+
     }
 
 }
