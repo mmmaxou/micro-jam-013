4b40c74d163fac9f691ebc49b6392556748a4c73
Pierrick Réveillère <reveillere.pierrick@gmail.com>
2024-05-05 16:28:28 +0200

    feat: add mud bar

--- Assets/Scripts/RockStockUI.cs
+++ Assets/Scripts/RockStockUI.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class RockStockUI : MonoBehaviour
 {
     public Text CounterText;
+    public Image Bar;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,9 @@ public class RockStockUI : MonoBehaviour
     void Update()
     {
         CounterText.text = GameManager.Instance.NbRock.ToString() + " / " + GameManager.Instance.MaxNbRock.ToString();
+        // Debug.Log(GameManager.Instance.NbRock / (float)GameManager.Instance.MaxNbRock);
+        // Bar.fillAmount = GameManager.Instance.NbRock / (float)GameManager.Instance.MaxNbRock;
+        // Bar.fillAmount = 0.5f;
+        Bar.fillAmount = (float)Math.Cos(Time.time) * 0.5f + 0.5f;
     }
 }
